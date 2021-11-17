601ea209594543ae6474664954c322a2cf080a19
Hamilton Santana <hamilton.santana@outlook.com>
2021-11-16 23:44:48 -0300

    refactor (API) - refatorando controllers para uso dos arquivos.

--- API/server.js
+++ API/server.js
@@ -2,14 +2,18 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-var orders = require('./routes/oders')
-var foods = require('./routes/foods')
-var users = require('./routes/users')
+const header = '<h1>Hello World</h1>';
+
+var orders = require('./routes/orders/oders.controller');
+var foods = require('./routes/foods/foods.controller');
+var users = require('./routes/users/users.controller');
 
 app.use(cors());
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-   res.send('Hello World')
+   res.send(header);
 });
 
 app.use('/foods', foods);
