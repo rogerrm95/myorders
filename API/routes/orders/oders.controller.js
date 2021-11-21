384ccf0512765aee00a7469c019ab92153f810de
rogerrm95 <rogerrm@live.com>
2021-11-20 23:37:37 -0300

    Fix: Geração de ID's únicos e rotas para update de pedidos

--- API/routes/orders/oders.controller.js
+++ API/routes/orders/oders.controller.js
@@ -1,38 +1,57 @@
 const fs = require('fs');
 const ordersJson = require('./orders.json');
-let _id_const = 1;
 var express = require('express');
 var orders = express.Router();
+
+function generateID(valueMax, listID) {
+    const data = new Date()
+    const month = data.getMonth() + 1
+    const value = Math.random() * valueMax
+    const id = `${month}${value.toFixed(0)}`.padEnd(5, "0")
+
+    const isExists = listID.lenght > 0 ? listID.every(item => item.id === id && true) : false
+
+    if (isExists) {
+        generateID(valueMax, listID)
+        return
+    }
+
+    return parseInt(id)
+}
 
 orders.get('/', (req, res) => {
     res.json(ordersJson);
 });
 
 orders.get('/:id', (req, res) => {
-    var resp = fs.readFileSync('./routes/orders/orders.json', 'utf-8', err => {
+    const resp = fs.readFileSync('./routes/orders/orders.json', 'utf-8', err => {
         if (err) {
             console.log('Error reading file', err)
         } else {
             console.log('Successfully read file')
         }
     });
-    var response = JSON.parse(resp);
-    var result = response.find(function (item, i) {
+
+    const response = JSON.parse(resp);
+    const result = response.find((item, i) => {
         if (item.id === Number(req.params.id)) {
-            index = i;
-            return i;
+            return item;
         }
     });
-    console.log(result);
-    res.status(200);
-    res.json(result);
+
+    res.status(200).json(result);
 });
 
 orders.post('/', (req, res) => {
-    req.body.id = _id_const;
-    _id_const += 1;
-    console.log(req.body);
-    ordersJson.push(req.body);
+
+    const id = generateID(500, ordersJson);
+
+    const data = {
+        ...req.body,
+        id
+    }
+
+    ordersJson.push(data);
     const jsonString = JSON.stringify(ordersJson, null, 2);
 
     fs.writeFile('./routes/orders/orders.json', jsonString, err => {
@@ -42,8 +61,44 @@ orders.post('/', (req, res) => {
             console.log('Successfully wrote file')
         }
     });
-    res.status(200);
-    res.json({ "id": req.body.id });
+
+    res.status(200).json({ "id": id });
 });
+
+orders.put('/:id', (req, res) => {
+    const id = Number(req.params.id)
+    
+    try {
+        const isExist = ordersJson.find(order => order.id === id && true)
+
+        if (!isExist) {
+            return res.status(404).send('Usuário não encontrado')
+        }
+
+        const data = req.body
+
+        const newList = ordersJson.map(order => {
+            if (order.id === id) {
+                return data
+            } else {
+                return order
+            }
+        })
+
+        const jsonString = JSON.stringify(newList, null, 2);
+        fs.writeFile('./routes/orders/orders.json', jsonString, err => {
+            if (err) {
+                console.log('Error writing file', err)
+            } else {
+                console.log('Successfully wrote file')
+            }
+        });
+
+        res.status(200).send(data);
+
+    } catch {
+        res.status(500).send('Erro durante o processamento!')
+    }
+})
 
 module.exports = orders;
