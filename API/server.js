4ed8d1029ecccb7cb7dd7dbf5a611abf0b9eaa0b
rogerrm95 <rogerrm@live.com>
2022-01-09 16:23:24 -0300

    Deploy Front-End

--- API/server.js
+++ API/server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-const header = '<h1>Hello World</h1>';
-
 var orders = require('./routes/orders/oders.controller');
 var foods = require('./routes/foods/foods.controller');
 var users = require('./routes/users/users.controller');
@@ -11,10 +9,6 @@ var users = require('./routes/users/users.controller');
 app.use(cors());
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-   res.send(header);
-});
 
 app.use('/foods', foods);
 
