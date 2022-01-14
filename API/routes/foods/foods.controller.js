3957d272173e36e3dfb46810cf5238d29b584f93
rogerrm95 <rogerrm@live.com>
2022-01-14 20:34:57 -0300

    Change: Remove API folder #3

--- API/routes/foods/foods.controller.js
+++
@@ -1,9 +0,0 @@
-const foodsJson = require('./foods.json');
-var express = require('express');
-var foods = express.Router();
-
-foods.get('/', (req, res) => {
-   res.json(foodsJson);
-});
-
-module.exports = foods;
