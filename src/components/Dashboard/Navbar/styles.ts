8fb7edfa2d438030d911a7ad4a4d2a687e7f4935
rogerrm95 <rogerrm@live.com>
2022-04-14 20:48:32 -0300

    Hotfix: alteração de css do navbar

--- src/components/Dashboard/Navbar/styles.ts
+++ src/components/Dashboard/Navbar/styles.ts
@@ -87,6 +87,7 @@ export const Container = styled.aside`
 
             h2 {
                 display: block;
+                color: var(--white);
             }
 
             hr {
@@ -118,6 +119,7 @@ export const Container = styled.aside`
             min-height: 22rem;
 
             background-color: #2A363B;
+            box-shadow: 0px 5px 5px rgba(0,0,0,0.25);
             z-index: 99999;
 
             & > :last-child{
