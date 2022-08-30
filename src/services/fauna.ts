54e1c506bc4dd5664dda5037216f38182f7ec132
rogerrm95 <rogerrm@live.com>
2022-08-29 22:23:32 -0300

    remove unnecessary libs

--- src/services/fauna.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from 'faunadb'
-
-export const Fauna = new Client({
-    secret: process.env.FAUNA_KEY as string,
-    domain: 'db.us.fauna.com',
-    scheme: 'https',
-    port: 443
-})
